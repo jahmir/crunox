79c545e2d5491c61585bf3f6708d9b877bad62ed
jahmir <altairjohnfbacc@gmail.com>
2021-04-08 12:23:59 +0800

    updates

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer className='footer'>
+        <footer className='footer mt-auto'>
             <Container>
                 <Row className='mt-auto'>
                     <Col className='text-center py-3'>Copyright &copy; Activity System by Altair</Col>
