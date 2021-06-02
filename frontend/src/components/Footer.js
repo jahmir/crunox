be526d8fffe6a97d6899f4a44d26edb66561ef2e
jahmir <altairjohnfbacc@gmail.com>
2021-06-02 21:22:28 +0800

    dev

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <footer className='footer mt-auto'>
             <Container>
                 <Row className='mt-auto'>
-                    <Col className='text-center py-3'>Copyright &copy; Activity System by Altair Footer develop</Col>
+                    <Col className='text-center py-3'>Copyright &copy; Activity System by Altair develop</Col>
                 </Row>
             </Container>
         </footer>
